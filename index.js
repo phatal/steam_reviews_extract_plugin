d5782e182d554f815675fb3e381bfc538ff681a1
phatal <phatal1988@gmail.com>
2023-07-21 16:49:44 +0700

    release 1.0

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-const https = require('https');
-const fs = require('fs');
-
 const express = require('express');
 const cors = require('cors');
 const srsRouter = require('./routes/steamreviewsextractor');
 const openaiRoutes = require('./routes/openai');
 
-const options = {
-    key: fs.readFileSync('./certs/private.key'),
-    cert: fs.readFileSync('./certs/www_hprnv_pro_2024_02_04.crt'),
-    ca: [
-        fs.readFileSync('./certs/root_pem_globalsign_ssl_dv_free_1.crt'),
-        fs.readFileSync('./certs/your-root-certificate.pem')
-    ]
-};
+
 
 const app = express();
 const PORT = 3000;
 
 // Setting CORS to allow chat.openapi.com is required for ChatGPT to access your plugin
-app.use(cors({ origin: [`http://localhost:${PORT}`, 'https://chat.openai.com'] }));
+app.use(cors({ origin: [`https://reviewextracting.hprnv.pro`, 'https://chat.openai.com'] }));
 app.use(express.json());
 
 // Simple request logging to see if your plugin is being called by ChatGPT
@@ -34,12 +24,7 @@ app.use(openaiRoutes);
 // The dummy steamreviewrsextractor API
 app.use('/reviews', srsRouter);
 
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Plugin server listening on port ${PORT}`)
-});
 
-/*
 app.listen(PORT, () => {
     console.log(`Plugin server listening on port ${PORT}`);
 });
-*/
